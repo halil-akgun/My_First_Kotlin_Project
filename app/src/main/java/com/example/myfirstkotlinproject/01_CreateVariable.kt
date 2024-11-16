eabce17671928ac45d85551dca2c0ee4ce221b28
Halil Akgun <halilakgun0@gmail.com>
2024-11-17 00:34:27 +0100

    create variable

---
+++ app/src/main/java/com/example/myfirstkotlinproject/01_CreateVariable.kt
@@ -0,0 +1,26 @@
+package com.example.myfirstkotlinproject
+
+fun main() {
+    // variable declaration
+    var studentName = "John"
+    var studentAge = 20
+    var studentHeight = 1.77
+    var letterGrade = 'A'
+
+    println(studentName)
+    println(studentAge)
+    println(studentHeight)
+    println(letterGrade)
+
+
+    // variable declaration with type
+    var productName: String = "Laptop"
+    var productPrice: Double = 1000.0
+    var productQuantity: Int = 10
+    var productDescription: String = "A high-performance laptop for work and play."
+
+    println(productName)
+    println(productPrice)
+    println(productQuantity)
+    println(productDescription)
+}
