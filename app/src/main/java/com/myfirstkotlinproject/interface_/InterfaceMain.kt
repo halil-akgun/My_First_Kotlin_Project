9ee9b1f1b0b947eb8c5e8ffe81879915635c4fc9
Halil Akgun <halilakgun0@gmail.com>
2024-11-29 00:13:38 +0100

    interface

--- app/src/main/java/com/myfirstkotlinproject/interface_/InterfaceMain.kt
+++ app/src/main/java/com/myfirstkotlinproject/interface_/InterfaceMain.kt
@@ -1,6 +1,31 @@
 package com.myfirstkotlinproject.interface_
 
 fun main() {
-    val amasyaApple = AmasyaApple()
-    amasyaApple.howToEat()
+    val lion = Lion()
+    val amasyaApple: Apple = AmasyaApple()
+    val apple = Apple()
+    val chicken: Eatable = Chicken()
+
+    val objectsList = listOf(lion, amasyaApple, apple, chicken)
+//    objectsList.forEach { item -> // A custom name like "item" can be assigned here
+    objectsList.forEach { // If no custom name is provided, "it" is used as the default identifier
+        if (it is Eatable) {
+            it.howToEat()
+        }
+
+        if (it is Squeezable) {
+            it.howToSqueeze()
+        }
+    }
+
+    val objectsArray = arrayOf(lion, amasyaApple, apple, chicken)
+    for (o in objectsArray) {
+        if (o is Eatable) {
+            o.howToEat()
+        }
+
+        if (o is Squeezable) {
+            o.howToSqueeze()
+        }
+    }
 }
---
+++ app/src/main/java/com/myfirstkotlinproject/interface_/Lion.kt
@@ -0,0 +1,4 @@
+package com.myfirstkotlinproject.interface_
+
+class Lion {
+}
